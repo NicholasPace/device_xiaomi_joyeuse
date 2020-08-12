0ad13157c00bc79676aa48b420772a60af93a0fb
skylark17 <axelancient1790@gmail.com>
2020-08-12 16:26:34 +0700

    joyeuse : BringUp RR-Ify Signed-off-by: Agung Pratama <id.agungpratama@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_joyeuse.mk
+    $(LOCAL_DIR)/rr_joyeuse.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_joyeuse-user \
-    lineage_joyeuse-userdebug \
-    lineage_joyeuse-eng
+    rr_joyeuse-user \
+    rr_joyeuse-userdebug \
+    rr_joyeuse-eng
