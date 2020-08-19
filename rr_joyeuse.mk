8db87044885d5c9d2397200ca74d5608bca4e08e
skylark17 <axelancient1790@gmail.com>
2020-08-19 09:19:27 +0700

    joyeuse : Delete Stock Fingerprint From Miui Signed-off-by: Agung Pratama <id.agungpratama@gmail.com>

--- rr_joyeuse.mk
+++ rr_joyeuse.mk
@@ -40,8 +40,6 @@ TARGET_VENDOR_PRODUCT_NAME := joyeuse
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="joyeuse-user 10 QKQ1.191215.002 V11.0.5.0.QJZMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/joyeuse_global/joyeuse:10/QKQ1.191215.002/V11.0.5.0.QJZMIXM:user/release-keys
-
 # Face Unlock
 export TARGET_FACE_UNLOCK_SUPPORT := true
 
