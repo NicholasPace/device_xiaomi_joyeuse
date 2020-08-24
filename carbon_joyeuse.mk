ef7320c3c421c3f905bf376dfef1236fcfc85639
Nicholas Pace <nicholaspace97@yahoo.ca>
2020-08-24 16:57:16 -0400

    Update and rename rr_joyeuse.mk to carbon_joyeuse.mk

--- carbon_joyeuse.mk
+++ carbon_joyeuse.mk
@@ -18,8 +18,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common RR-OS stuff
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+# inherit from vendor
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Inherit from joyeuse device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +31,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := joyeuse
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := rr_joyeuse
+PRODUCT_NAME := carbon_joyeuse
 PRODUCT_MODEL := Redmi Note 9 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
